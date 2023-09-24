d21d5432b6e85c78bae607c6a8d0e587e79c218e
samuelj123 <samuelj123@gmail.com>
2023-09-25 09:45:22 +1000

    little bit of adding html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+	<nav>
+		<ul>
+			<li>Who We are</li>
+			<li>Ministries</li>
+			<li>Latest Messages</li>
+		</ul>
+		<ul class="whoweare">
+			<li>The Baptist Church</li>
+			<li>History of CBC Nanango</li>
+			<li>Our Pastors</li>
+		</ul>
+		<ul class="ministries">
+			<li>Community Op-Shops</li>
+			<li>Bus trips</li>
+			<li>Overseas Mission</li>
+		</ul>
+	</nav>
+	<h1>Community Baptist Church</h1>
+	<h3>Nanango</h3>
+	<h5>Join us at Church</h5>
+	<h4>Sundays 10:00am</h4>
+	<button class = "cta">Pastor Bill: +65 xxx xxx xxx</button>
+	<button class="cta">Church Activities</button>
+</section>
